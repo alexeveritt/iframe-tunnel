aeca14960b09f902c3121bee96db92147730ff0c
Alex Everitt <alex.everitt@bytechaser.com>
2018-06-22 19:16:23 +0100

    updating host tunnel

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ export interface Tunnel {
     sendMessage(key: string, data?: string | object): void
 }
 
-interface TunnelOptions {
+export interface TunnelOptions {
     iframeId?: string
     targetOrigin?: string
 }
@@ -28,7 +28,7 @@ export class ClientTunnel extends JSEmitter implements Tunnel {
 
     constructor(options: TunnelOptions) {
         super();
-        window.addEventListener("message", this.onMessage, false);
+       // window.addEventListener("message", this.onMessage, false);
     }
 
     public sendMessage(key: string, data?: string | object): void {
@@ -41,14 +41,19 @@ export class HostTunnel extends JSEmitter implements Tunnel {
     private isTunnelReady = false;
     private eventQueue: IQueueEvent[] = [];
     private targetOrigin = '*';
-    private iframeId = undefined;
-
+    private readonly iframeId: string;
+    private iframeElement: HTMLIFrameElement
     private reservedKeys = {
         __jstunnel_ready: 1
     };
 
     constructor(options: TunnelOptions) {
         super();
+        if (!options.iframeId) {
+            throw new Error('No Iframe Id')
+        }
+        this.iframeId = options.iframeId;
+
         this.on('__jstunnel_ready', this.onReady)
         window.addEventListener("message", this.onMessage, false);
     }
@@ -68,8 +73,14 @@ export class HostTunnel extends JSEmitter implements Tunnel {
     }
 
     private processQueueEvent(evt: IQueueEvent): void {
-        const el = window.document.getElementById(this.iframeId) as any;
-        el.contentWindow.postMessage(evt.payload, this.targetOrigin);
+        if (this.iframeId) {
+            if (!this.iframeElement) {
+                this.iframeElement = window.document.getElementById(this.iframeId) as HTMLIFrameElement;
+            }
+            if (this.iframeElement && this.iframeElement.contentWindow) {
+                this.iframeElement.contentWindow.postMessage(evt.payload, this.targetOrigin);
+            }
+        }
     }
 
     private processQueuedEvents(): void {
@@ -85,13 +96,15 @@ export class HostTunnel extends JSEmitter implements Tunnel {
         if (this.targetOrigin !== '*' && event.origin !== this.targetOrigin) {
             return;
         }
+
         if (event.data) {
             try {
+                // convert to string or object // add some meta to describe type
                 const message = JSON.parse(event.data);
                 this.emit(message.key, message.data);
             }
             catch (ex) {
-
+                // probably invalid json data
             }
         }
     }
