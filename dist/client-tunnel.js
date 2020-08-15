6e7bedc9d0d666970a5d73dea34bbce7447a9468
Alex Everitt <alex.everitt@bytechaser.com>
2020-08-15 11:08:39 +0100

    update typescript

--- dist/client-tunnel.js
+++ dist/client-tunnel.js
@@ -1,8 +1,11 @@
 "use strict";
 var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -11,9 +14,9 @@ var __extends = (this && this.__extends) || (function () {
 })();
 Object.defineProperty(exports, "__esModule", { value: true });
 var jsemitter_1 = require("jsemitter");
+var events_1 = require("./utils/events");
 var logger_1 = require("./utils/logger");
 var packer_1 = require("./utils/packer");
-var events_1 = require("./utils/events");
 var ClientTunnel = (function (_super) {
     __extends(ClientTunnel, _super);
     function ClientTunnel(options) {
